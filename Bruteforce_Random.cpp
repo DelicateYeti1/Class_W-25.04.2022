a5d582c7c4c2dbf7db0878f292575c4c55d89128
Okhlopkov Konstantin <okl231003@gmail.com>
2022-04-25 10:23:30 +1000

    1-comm

---
+++ Bruteforce_Random.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int g = 0, i = 0, a = 0;
+	srand(time(NULL));
+	cout << "Lenght of password: " << endl;
+	cin >> a;
+	char ps[a], randps[a];
+	cout << "Password: " << endl;
+	for (int j = 0; j < a; j++)
+	{
+		cin >> ps[j];
+	}
+	
+	for (int j = 0; j < a; j++)
+	{
+		while (ps[j] != i)
+		{
+			i = (rand()%94 + 32);
+			g++;	
+		}
+		randps[j] = i;
+	}
+	
+	cout << "Selectioned password: " << endl;
+	for (int j = 0; j < a; j++)
+	{
+		cout << randps[j];
+	}
+	cout << endl;
+	cout << "Number of interations: " << g-1;
+}
